babb81f4bcc3c95e99a5c66ed26f45e9b5a7fc5f
badboy718 <13070101975@139.com>
2026-07-02 23:00:06 +0800

    fix: 修正面向对象笔记代码错误，删除空网络编程文件

--- exercises/test.py
+++ exercises/test.py
@@ -1,15 +1,20 @@
-class Account():
-    def __init__(self,name):
-        self.name = name
-        self.__money = 100
-    def get_money(self):
-        return self.__money
-    def set_money(self,money):
-        self.__money = money
+def fn1(fn):
+    print("fn1")
+    def fn2(num1, num2):
+        print("fn2", num1, num2)
+        res=fn(num1, num2)
+        print(res)
+    return fn2
 
-user=Account('金城武')
-print(user.name)
-#print(user.money)
-print(user.get_money())
-user.set_money(200)
-print(user.get_money())
+@fn1
+def fn3(n1,n2):
+    print("fn3")
+    return n1+n2
+
+@fn1
+def fn4(n1,n2):
+    print("fn4")
+    return n1+n2
+
+fn3(10,20)
+fn4(50,100)
